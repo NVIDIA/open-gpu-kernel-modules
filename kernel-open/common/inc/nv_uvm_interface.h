9f9c791f18a9e433cc07b7623abe60921163f4f2
Sourajyoti Basak <wiz28@protonmail.com>
2022-05-12 12:42:18 +0530

    refactor(nv_uvm_interface): fix typos

--- kernel-open/common/inc/nv_uvm_interface.h
+++ kernel-open/common/inc/nv_uvm_interface.h
@@ -972,7 +972,7 @@ NV_STATUS nvUvmInterfaceEnableAccessCntr(uvmGpuDeviceHandle device,
 /*******************************************************************************
     nvUvmInterfaceDisableAccessCntr
 
-    This function disables acccess counters
+    This function disables access counters
     UVM is also returning ownership to the RM: RM can service, enable or
     disable this interrupt. In this case, access counter notificaion interrupts
     are disabled by this function before it returns.
@@ -1280,8 +1280,8 @@ NV_STATUS nvUvmInterfacePagingChannelsMap(uvmGpuAddressSpaceHandle srcVaSpace,
     RM or GPU locks.
 
     Arguments:
-        srcVaSpace[IN] - VA space handle that was passed to prevous mapping.
-        srcAddress[IN] - virtual address that was passed to prevous mapping.
+        srcVaSpace[IN] - VA space handle that was passed to previous mapping.
+        srcAddress[IN] - virtual address that was passed to previous mapping.
         device[IN]     - device under which paging channels were allocated.
  */
 void nvUvmInterfacePagingChannelsUnmap(uvmGpuAddressSpaceHandle srcVaSpace,
