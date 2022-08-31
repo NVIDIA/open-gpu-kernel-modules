91e02299dcb976881e3a0fc579d35c34decde85c
Liam Middlebrook <lmiddlebrook@nvidia.com>
2022-08-31 16:26:10 -0700

    515.49.15

--- src/common/inc/nvBldVer.h
+++ src/common/inc/nvBldVer.h
@@ -43,18 +43,18 @@
 #endif
 
 #if defined(NV_LINUX) || defined(NV_BSD) || defined(NV_SUNOS)
-#define NV_BUILD_BRANCH_VERSION         "rel/gpu_drv/r515/VK516_10-213"
-#define NV_BUILD_CHANGELIST_NUM         (31711086)
+#define NV_BUILD_BRANCH_VERSION         "rel/gpu_drv/r515/VK516_10-214"
+#define NV_BUILD_CHANGELIST_NUM         (31748934)
 #define NV_BUILD_TYPE                   "Official"
-#define NV_BUILD_NAME                   "rel/gpu_drv/r515/VK516_10-213"
-#define NV_LAST_OFFICIAL_CHANGELIST_NUM (31711086)
+#define NV_BUILD_NAME                   "rel/gpu_drv/r515/VK516_10-214"
+#define NV_LAST_OFFICIAL_CHANGELIST_NUM (31748934)
 
 #else     /* Windows builds */
-#define NV_BUILD_BRANCH_VERSION         "VK516_10-14"
-#define NV_BUILD_CHANGELIST_NUM         (31711086)
+#define NV_BUILD_BRANCH_VERSION         "VK516_10-18"
+#define NV_BUILD_CHANGELIST_NUM         (31748934)
 #define NV_BUILD_TYPE                   "Official"
-#define NV_BUILD_NAME                   "517.11"
-#define NV_LAST_OFFICIAL_CHANGELIST_NUM (31711086)
+#define NV_BUILD_NAME                   "517.22"
+#define NV_LAST_OFFICIAL_CHANGELIST_NUM (31748934)
 #define NV_BUILD_BRANCH_BASE_VERSION    R515
 #endif
 // End buildmeister python edited section
