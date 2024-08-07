db8452428c0c00e5b49cd13bb2cab736cb0149f0
russellcnv <russellc@nvidia.com>
2024-08-06 17:23:42 -0700

    550.40.67

--- src/common/inc/nvBldVer.h
+++ src/common/inc/nvBldVer.h
@@ -43,18 +43,18 @@
 #endif
 
 #if defined(NV_LINUX) || defined(NV_BSD) || defined(NV_SUNOS)
-#define NV_BUILD_BRANCH_VERSION         "rel/gpu_drv/r550/VK551_06-144"
-#define NV_BUILD_CHANGELIST_NUM         (34477894)
+#define NV_BUILD_BRANCH_VERSION         "rel/gpu_drv/r550/VK551_06-146"
+#define NV_BUILD_CHANGELIST_NUM         (34676523)
 #define NV_BUILD_TYPE                   "Official"
-#define NV_BUILD_NAME                   "rel/gpu_drv/r550/VK551_06-144"
-#define NV_LAST_OFFICIAL_CHANGELIST_NUM (34477894)
+#define NV_BUILD_NAME                   "rel/gpu_drv/r550/VK551_06-146"
+#define NV_LAST_OFFICIAL_CHANGELIST_NUM (34676523)
 
 #else     /* Windows builds */
-#define NV_BUILD_BRANCH_VERSION         "VK551_06-17"
-#define NV_BUILD_CHANGELIST_NUM         (34477894)
+#define NV_BUILD_BRANCH_VERSION         "VK551_06-19"
+#define NV_BUILD_CHANGELIST_NUM         (34676523)
 #define NV_BUILD_TYPE                   "Official"
-#define NV_BUILD_NAME                   "552.79"
-#define NV_LAST_OFFICIAL_CHANGELIST_NUM (34477894)
+#define NV_BUILD_NAME                   "552.93"
+#define NV_LAST_OFFICIAL_CHANGELIST_NUM (34676523)
 #define NV_BUILD_BRANCH_BASE_VERSION    R550
 #endif
 // End buildmeister python edited section
