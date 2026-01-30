a20d9e76676e73b1ff1d26f1f38da75545855223
russellcnv <russellc@nvidia.com>
2026-01-30 10:49:02 -0800

    580.94.17

--- src/common/inc/nvBldVer.h
+++ src/common/inc/nvBldVer.h
@@ -43,18 +43,18 @@
 #endif
 
 #if defined(NV_LINUX) || defined(NV_BSD) || defined(NV_SUNOS)
-#define NV_BUILD_BRANCH_VERSION         "rel/gpu_drv/r580/VK580_65-192"
-#define NV_BUILD_CHANGELIST_NUM         (37211220)
+#define NV_BUILD_BRANCH_VERSION         "rel/gpu_drv/r580/VK580_65-193"
+#define NV_BUILD_CHANGELIST_NUM         (37259058)
 #define NV_BUILD_TYPE                   "Official"
-#define NV_BUILD_NAME                   "rel/gpu_drv/r580/VK580_65-192"
-#define NV_LAST_OFFICIAL_CHANGELIST_NUM (37211220)
+#define NV_BUILD_NAME                   "rel/gpu_drv/r580/VK580_65-193"
+#define NV_LAST_OFFICIAL_CHANGELIST_NUM (37259058)
 
 #else     /* Windows builds */
-#define NV_BUILD_BRANCH_VERSION         "VK580_65-17"
-#define NV_BUILD_CHANGELIST_NUM         (37211220)
+#define NV_BUILD_BRANCH_VERSION         "VK580_65-19"
+#define NV_BUILD_CHANGELIST_NUM         (37259058)
 #define NV_BUILD_TYPE                   "Official"
-#define NV_BUILD_NAME                   "582.29"
-#define NV_LAST_OFFICIAL_CHANGELIST_NUM (37211220)
+#define NV_BUILD_NAME                   "582.32"
+#define NV_LAST_OFFICIAL_CHANGELIST_NUM (37259058)
 #define NV_BUILD_BRANCH_BASE_VERSION    R580
 #endif
 // End buildmeister python edited section
